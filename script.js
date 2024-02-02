61a8e02988b2b074d3eccb88ca06cf22421a1de8
maharthawijaya <63789469+maharthawijaya@users.noreply.github.com>
2024-02-02 21:32:11 +0800

    Initial commit
    feat: play/pause, previous, next. add 3 songs

---
+++ script.js
@@ -0,0 +1,75 @@
+const music = document.querySelector('audio');
+const previousButton = document.getElementById('prev');
+const playButton = document.getElementById('main-button');
+const nextButton = document.getElementById('next');
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist')
+
+// Music
+const songs = [
+    {
+        name: 'pokemon-1',
+        displayName: 'Pokemon Ruby & Sapphire Music Medley',
+        artist: 'Dr. Pez & VGM',
+    },
+    {
+        name: 'pokemon-2',
+        displayName: 'Pokemon Littleroot Town Theme Music',
+        artist: 'Pokemon',
+    },
+    {
+        name: 'pokemon-3',
+        displayName: 'Pokemon Driftveil City Theme Music',
+        artist: 'Pokemon',
+    }
+];
+
+let isPlaying = false;
+
+function PlayMusic(){
+    isPlaying = true
+    playButton.classList.replace('fa-play', 'fa-pause');
+    playButton.setAttribute('title', 'Pause');
+    music.play();
+}
+
+function PauseMusic(){
+    isPlaying = false;
+    playButton.classList.replace('fa-pause', 'fa-play');
+    playButton.setAttribute('title', 'Play');
+    music.pause();
+}
+
+playButton.addEventListener('click', () => (isPlaying ? PauseMusic() : PlayMusic()));
+
+let songIndex = 0;
+
+function LoadMusic(song){
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+function NextSong(){
+    songIndex++;
+    if(songIndex > songs.length - 1){
+        songIndex = 0;
+    }
+    LoadMusic(songs[songIndex]);
+    PlayMusic();
+}
+function PreviousSong(){
+    songIndex--;
+    if(songIndex < 0){
+        songIndex = songs.length-1;
+    }
+    LoadMusic(songs[songIndex]);
+    PlayMusic();
+}
+
+LoadMusic(songs[songIndex]);
+
+previousButton.addEventListener('click', PreviousSong);
+nextButton.addEventListener('click', NextSong)
