1978c6af81339692dcc88ba3a28a7aad81a15e7a
maharthawijaya <63789469+maharthawijaya@users.noreply.github.com>
2024-02-02 22:56:33 +0800

    Initial Commit
    feat: songs current time and duration

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const playButton = document.getElementById('main-button');
 const nextButton = document.getElementById('next');
 const image = document.querySelector('img');
 const title = document.getElementById('title');
-const artist = document.getElementById('artist')
+const artist = document.getElementById('artist');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+const currentTimeElement = document.getElementById('current-time');
+const durationElement = document.getElementById('duration');
 
 // Music
 const songs = [
@@ -69,7 +73,35 @@ function PreviousSong(){
     PlayMusic();
 }
 
+function UpdateProgressBar(e){
+    if(isPlaying){
+        const { currentTime, duration } = e.srcElement;
+
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+
+        const durationMinutes = Math.floor(duration/60);
+        let durationSeconds = Math.floor(duration%60);
+        if(durationSeconds < 10){
+            durationSeconds = `0${durationSeconds}`;
+        }
+        if(durationSeconds){
+            durationElement.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+
+        const currentMinutes = Math.floor(currentTime/60);
+        let currentSeconds = Math.floor(currentTime%60);
+        if(currentSeconds < 10){
+            currentSeconds = `0${currentSeconds}`;
+        }
+        if(currentSeconds){
+            currentTimeElement.textContent = `${currentMinutes}:${currentSeconds}`;
+        }
+    }
+}
+
 LoadMusic(songs[songIndex]);
 
 previousButton.addEventListener('click', PreviousSong);
-nextButton.addEventListener('click', NextSong)
+nextButton.addEventListener('click', NextSong);
+music.addEventListener('timeupdate', UpdateProgressBar);
